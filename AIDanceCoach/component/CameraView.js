232516fd393c69c59e4c953e159406134ed400c1
홍재이 <kitiyj526@gmail.com>
2019-11-19 19:50:51 +0900

    Rebuild react-native-app because of link error

---
+++ AIDanceCoach/component/CameraView.js
@@ -0,0 +1,60 @@
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+class CameraView extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+        >
+          <Text style={styles.capture} onPress={this.takePicture}>[CAPTURE]</Text>
+        </RNCamera>
+      </View>
+    );
+  }
+
+  takePicture = async() => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+
+export default CameraView;
