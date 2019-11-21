5626c37a76efd9ee9855510401a11c7eb3412f5a
SimonZocholl <36897182+SimonZocholl@users.noreply.github.com>
2019-11-21 20:33:06 +0900

    test VideoExtractor, (in progress 7)

--- Backend/videoextractor/VideoExtractor.py
+++ Backend/videoextractor/VideoExtractor.py
@@ -13,6 +13,9 @@ class VideoExtractor:
         self.video_name = video_name
         self.video_path = os.path.join(video_dir, video_name)
         self.media_dir = media_dir
+        self.picture_dir = os.path.join(self.media_dir, "pictures")
+        self.skeleton_dir = os.path.join(self.media_dir, "skeletons")
+
         self.video = cv2.VideoCapture(self.video_path)
         self.predictor = PosePredictor(model=model_path, disable_blending=True)
         self.frequency = 1 / framerate
@@ -43,16 +46,13 @@ class VideoExtractor:
 
     #  it will capture image in each 0.5 second
     def _sample_pictures(self):
-        print(1.1)
-        picture_dir = os.path.join(self.media_dir, "pictures")
-        self.clear_dir(picture_dir)
-        print(1.2)
+        self.clear_dir(self.picture_dir)
 
         def get_frame(sec):
             self.video.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
             has_frames, image = self.video.read()
             if has_frames:
-                cv2.imwrite(os.path.join(picture_dir, str(count) + ".jpg"), image)  # save frame as JPG file
+                cv2.imwrite(os.path.join(self.picture_dir, str(count) + ".jpg"), image)  # save frame as JPG file
             return has_frames
 
         sec = 0
@@ -60,25 +60,23 @@ class VideoExtractor:
         success = get_frame(sec)
 
         while success:
-            print(1.3)
             count += 1
             sec += self.frequency
             sec = round(sec, 2)
             success = get_frame(sec)
 
     def _extract_keypoints(self):
-
         # put in loop
-        datum = self.predictor.predict_image('images/1.jpg')
-
-        print("Body keypoints: \n" + str(datum.poseKeypoints))
-        print("Face keypoints: \n" + str(datum.faceKeypoints))
-        print("Left hand keypoints: \n" + str(datum.handKeypoints[0]))
-        print("Right hand keypoints: \n" + str(datum.handKeypoints[1]))
-
-        cv2.imwrite("test.jpg", datum.cvOutputData)  # skeleton - Black background
-
-        pass
+        print(2.1)
+        for i, pictures in enumerate(os.listdir(self.picture_dir)):
+            print(2.2)
+            datum = self.predictor.predict_image(os.path.join(self.picture_dir, pictures))
+            print("Body keypoints: \n" + str(datum.poseKeypoints))
+            print("Face keypoints: \n" + str(datum.faceKeypoints))
+            print("Left hand keypoints: \n" + str(datum.handKeypoints[0]))
+            print("Right hand keypoints: \n" + str(datum.handKeypoints[1]))
+            cv2.imwrite(os.path.join(self.skeleton_dir, str(i) + ".jpg"), datum.cvOutputData)
+        print(2.3)
 
     def _overlay_images(self):
         # https://stackoverflow.com/questions/38627870/how-to-paste-a-png-image-with-transparency-to-another-image-in-pil-without-white
