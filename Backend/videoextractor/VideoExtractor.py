b1b998a91bae825dbe33fbdca809a2663a3b10b0
SimonZocholl <36897182+SimonZocholl@users.noreply.github.com>
2019-11-22 12:46:58 +0900

    Fix for Overlay generation

--- Backend/videoextractor/VideoExtractor.py
+++ Backend/videoextractor/VideoExtractor.py
@@ -6,6 +6,17 @@ import codecs
 from PIL import Image
 
 from posewrapper.PosePredictor import PosePredictor
+'''
+    media
+    -> 0
+        -> video
+        -> skeleton
+        -> ...
+    -> 1
+        -> video
+        -> skeleton
+        -> ...
+'''
 
 
 class VideoExtractor:
@@ -15,10 +26,11 @@ class VideoExtractor:
                  framerate=-1):
         self.video_name = video_name
         self.video_path = os.path.join(video_dir, video_name)
+
         self.media_dir = media_dir
         self.picture_dir = os.path.join(self.media_dir, "pictures")
         self.skeleton_dir = os.path.join(self.media_dir, "skeletons")
-        self.body_dir = os.path.join(self.media_dir, "bodies")
+        self.body_keypoints_dir = os.path.join(self.media_dir, "bodies_keypoints")
         self.overlay_dir = os.path.join(self.media_dir, "overlays")
 
         self.video = cv2.VideoCapture(self.video_path)
@@ -28,7 +40,7 @@ class VideoExtractor:
     def __call__(self, *args, **kwargs):
         return self.extract(*args, **kwargs)
 
-    def create_and_clear(self, directory):
+    def clear_and_create(self, directory):
         if os.path.exists(directory):
             shutil.rmtree(directory, ignore_errors=True)
         os.makedirs(directory)
@@ -36,7 +48,7 @@ class VideoExtractor:
     def extract(self):
         self.video = cv2.VideoCapture(self.video_path)
         # clear previous media with same id
-        self.create_and_clear(self.media_dir)
+        self.clear_and_create(self.media_dir)
 
         self._sample_pictures()
         self._extract_keypoints()
@@ -46,7 +58,7 @@ class VideoExtractor:
 
     #  it will capture image in each 0.5 second
     def _sample_pictures(self):
-        self.create_and_clear(self.picture_dir)
+        self.clear_and_create(self.picture_dir)
 
         def get_frame(sec):
             self.video.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
@@ -67,29 +79,25 @@ class VideoExtractor:
 
     def _extract_keypoints(self):
         # put in loop
-        self.create_and_clear(self.skeleton_dir)
-        self.create_and_clear(self.body_dir)
+        self.clear_and_create(self.skeleton_dir)
+        self.clear_and_create(self.body_keypoints_dir)
         for i, pictures in enumerate(os.listdir(self.picture_dir)):
             datum = self.predictor.predict_image(os.path.join(self.picture_dir, pictures))
             datum_list = datum.poseKeypoints.tolist()
-            json.dump(datum_list, codecs.open(os.path.join(self.body_dir, str(i) + ".json"), 'w', encoding='utf-8'),
+            json.dump(datum_list,
+                      codecs.open(os.path.join(self.body_keypoints_dir, str(i) + ".json"), 'w', encoding='utf-8'),
                       separators=(',', ':'))  ### this saves the array in .json format
             cv2.imwrite(os.path.join(self.skeleton_dir, str(i) + ".jpg"), datum.cvOutputData)
-            '''
-            print("Body keypoints: \n" + str(datum.poseKeypoints))
-            print("Face keypoints: \n" + str(datum.faceKeypoints))
-            print("Left hand keypoints: \n" + str(datum.handKeypoints[0]))
-            print("Right hand keypoints: \n" + str(datum.handKeypoints[1]))
-            '''
+
 
     def _overlay_images(self):
-        self.create_and_clear(self.overlay_dir)
+        self.clear_and_create(self.overlay_dir)
         for i, (pic, ske) in enumerate(zip(os.listdir(self.picture_dir), os.listdir(self.skeleton_dir))):
             picture = Image.open(os.path.join(self.picture_dir, pic), 'r')
             skeleton = Image.open(os.path.join(self.skeleton_dir, ske), 'r')
-            overlay = Image.new(mode='RGB', size=picture.size)
+            overlay = Image.new('RGBA', picture.size, (0, 0, 0, 0))
             overlay.paste(picture, (0, 0))
-            overlay.paste(skeleton, (0, 0))
+            overlay.paste(skeleton, (0, 0), mask=skeleton)
             overlay.save(os.path.join(self.overlay_dir, str(i) + ".jpg"), format="JPEG")
         # https://stackoverflow.com/questions/38627870/how-to-paste-a-png-image-with-transparency-to-another-image-in-pil-without-white
 
