d0e4258a154f69c73ee3520412309cd478c42486
SimonZocholl <36897182+SimonZocholl@users.noreply.github.com>
2019-11-27 14:41:00 +0900

    added building of Videos, building a tar.gz repose, sending it back (untested)

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, Response
 import os
 import json
-
 from extract_video.VideoExtractor import VideoExtractor
 from build_response.ResponseBuilder import ResponseBuilder
+from flask import send_from_directory
 
 app = Flask(__name__)
 VIDEO_DIR = './video'
-
 app.secret_key = "super secret key"
 app.config["UPLOAD_FOLDER"] = VIDEO_DIR
 
-# pred.predict_image(image_path) for inference
 
 @app.route('/')
 def hello_world():
@@ -19,10 +17,9 @@ def hello_world():
 
 
 # receives and stores file using for example
-# curl -X POST -F file=@"/mnt/c/Users/nomis/Desktop/testvideo1.mp4" http://127.0.0.1:5000
+# curl -X POST -F file=@"/path/to/file.mp4" -F file=@"/path/to/file.json" http://208.43.39.216:5000
 @app.route('/', methods=['POST'])
 def video_in():
-    # video_file = request.files['file']
     video_file, video_name, json_file, json_name = '', '', '', ''
 
     for f in request.files.getlist("file"):
@@ -35,8 +32,8 @@ def video_in():
 
     is_sample = False
     config = json.load(json_file)
-    is_sample = config['is_sample'] # <- boolean
-    compare_to = config['compare_to'] # <-- samplevideo
+    is_sample = config['is_sample']  # <- boolean
+    compare_to = config['compare_to']  # <-- samplevideo
 
     video_path = os.path.join(VIDEO_DIR, video_name)
     if not os.path.exists(VIDEO_DIR):
@@ -44,19 +41,16 @@ def video_in():
     input_path = os.path.join(VIDEO_DIR, video_name)
     video_file.save(input_path)
 
-    print(f'is_sample: {is_sample}')
-    print(f'video_path: {video_path}')
-    print(f'video_name: {video_name}')
-    print(f'compare_to: {compare_to}')
-
     if is_sample:
         vd = VideoExtractor(media_dir="./media", model_path="../../openpose/models/")  # framerate > 1 !!!
         body_points = vd.extract(video_path, video_name.split('.')[0], framerate=30)
         return Response(status=200, response="Added sample")
     else:
-        rb = ResponseBuilder(input_path=video_path,sample_id=video_name.split('.')[0])  # video_path: attempt, sample_id: sample
-        result = rb.build()
-        return Response(status=200, response=json.dumps(result))
+        rb = ResponseBuilder(input_path=video_path,
+                             sample_id=video_name.split('.')[0])  # video_path: attempt, sample_id: sample
+        result_dir = rb.build()
+        # return send_from_directory(result, filename, as_attachment=True)
+        return send_from_directory(result_dir, 'result.tar.gz', as_attachment=True)
 
 
 if __name__ == '__main__':
--- Backend/build_response/ResponseBuilder.py
+++ Backend/build_response/ResponseBuilder.py
@@ -1,4 +1,7 @@
 from build_response.analyze_movement.MovementAnalyzer import MovementAnalyzer
+import cv2
+import os
+import tarfile
 
 
 class ResponseBuilder:
@@ -6,13 +9,12 @@ class ResponseBuilder:
         self.input_path = input_path
         self.analyze = MovementAnalyzer(sample_id)
         self.data = list()
+        self.result_dir = '.media/result'
 
     def build(self):
         self.data = self.analyze(self.input_path)
-
         threshold = 10
         frame_radius = 2  # number of frames to display before and after
-
         index_of_failure = 0
 
         try:
@@ -20,17 +22,51 @@ class ResponseBuilder:
         except Exception:
             pass
 
-        start = index_of_failure-frame_radius if index_of_failure > frame_radius else 0
-        end = index_of_failure - frame_radius if len(self.data) > frame_radius+index_of_failure else len(self.data)-1
-
-        vis = self.visualize(start, end)
+        start = index_of_failure - frame_radius if index_of_failure > frame_radius else 0
+        end = index_of_failure - frame_radius if len(self.data) > frame_radius + index_of_failure else len(
+            self.data) - 1
+        result_dict = self.visualize(start, end)
+        tar_path = os.path.join(self.result_dir, 'result.tar.gz')
+        tar = tarfile.open(tar_path, 'w:gz')
+        # add json in required
+        tar.add(result_dict['sample_result_path'])
+        tar.add(result_dict['trail_result_path'])
+        tar.close()
         """
         TODO append visualization and raw_data for sending back
         """
-        return self.data
+        return tar_path
 
-    def visualize(self, start, end):
+    # expects 2 lists of file paths to the correct files
+    def visualize(self, frames_trail, frames_sample):
+        array_trail = []
+        array_sample = []
+        # add path to files if necessary
+        for i, (file_trail, file_sample) in enumerate(zip(frames_trail, frames_sample)):
+            picture_trail = cv2.imread(file_trail, 'r')
+            array_trail.append(picture_trail)
+            picture_sample = cv2.imread(file_sample, 'r')
+            array_sample.append(picture_sample)
+
+        size = array_trail[0].shape[1::-1]
+
+        # where to store the files?
+        trail_result_path = os.path.join(self.result_dir, 'trail.avi')
+        out_trail = cv2.VideoWriter(trail_result_path,
+                                    cv2.VideoWriter_fourcc(*'DIVX'),
+                                    fps=30, frame_size=size)
+        map(out_trail.write, array_trail)
+        out_trail.release()
+        sample_result_path = os.path.join(self.result_dir, 'sample.avi')
+        out_sample = cv2.VideoWriter(sample_result_path,
+                                     cv2.VideoWriter_fourcc(*'DIVX'),
+                                     fps=30, frame_size=size)
+        map(out_trail.write, array_sample)
+        out_sample.release()
+
         # return visualization???
         # TODO basic loading of corresponding video frames
         # TODO 2 add scaled visualization (how to access transformation matrix in a clean way?)
-        return "dummy"
+        return {'sample_result_path': sample_result_path,
+                'trail_result_path': trail_result_path
+                }
