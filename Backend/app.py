7f764607b4323f40b7010aa1fc3484979e6175c0
SimonZocholl <36897182+SimonZocholl@users.noreply.github.com>
2019-11-28 18:29:19 +0900

    fix

--- Backend/app.py
+++ Backend/app.py
@@ -51,6 +51,8 @@ def process_videos():
         rb = ResponseBuilder(input_path=input_path,
                              sample_id=compare_to.split('.')[0])  # video_path: attempt, sample_id: sample
         result_path = rb.build()
+        print(f'result_path: {result_path}')
+
         return send_from_directory(result_path, 'result.tar.gz', as_attachment=True)
 
 
