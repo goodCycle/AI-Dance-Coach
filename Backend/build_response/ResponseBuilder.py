f4347b37de592f1a2d3599bf25b79247273f9041
SimonZocholl <36897182+SimonZocholl@users.noreply.github.com>
2019-11-28 15:10:42 +0900

    fix

--- Backend/build_response/ResponseBuilder.py
+++ Backend/build_response/ResponseBuilder.py
@@ -45,8 +45,7 @@ class ResponseBuilder:
         print(f'trail_result_path:{result_dict["trail_result_path"]}')
 
         # todo: fix
-        os.makedirs(tar_path)
-        with tarfile.open(tar_path, 'w:gz')as tar:
+        with tarfile.open(tar_path, 'x:gz')as tar:
             tar.add(result_dict['sample_result_path'])
             tar.add(result_dict['trail_result_path'])
         return tar_path
