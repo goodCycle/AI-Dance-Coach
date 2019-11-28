8666e02f8f0aff6450da569a600fdc28268bad42
홍재이 <kitiyj526@gmail.com>
2019-11-29 00:15:19 +0900

    Store video on phone local

--- AIDanceCoach/component/CameraView.js
+++ AIDanceCoach/component/CameraView.js
@@ -10,8 +10,8 @@ import {
 
 import { RNCamera } from 'react-native-camera';
 import CameraRoll from '@react-native-community/cameraroll';
+import ImagePicker from 'react-native-image-picker';
 import Config from 'react-native-config';
-import RNFetchBlob from 'rn-fetch-blob';
 
 const RNFS = require('react-native-fs');
 
@@ -28,6 +28,7 @@ const styles = StyleSheet.create({
   recordButtonContainer: {
     flex: 0.2,
     flexDirection: 'column',
+    alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#B82303',
   },
@@ -44,6 +45,10 @@ const styles = StyleSheet.create({
     color: '#B82303',
     fontWeight: 'bold',
   },
+  horizontalButtonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
 
 class CameraView extends Component {
@@ -55,10 +60,10 @@ class CameraView extends Component {
   getInitialState() {
     return {
       recording: false,
-      processing: false,
       hasOpenPoseResult: false,
       openPoseResult: null,
-      recentVideoUri: null,
+      recordedVideoUri: null,
+      recordedVideoCodec: null,
     };
   }
 
@@ -73,32 +78,39 @@ class CameraView extends Component {
     return videoUri;
   }
 
-  startRecording = async () => {
-    this.setState({ recording: true });
-    // default to mp4 for android as codec is not set
-    const { uri, codec = 'H264' } = await this.camera.recordAsync();
-    const videoUri = this.saveVideoToCameraRoll(uri);
-    this.setState({ recentVideoUri: videoUri });
-    this.setState({ recording: false, processing: true });
-    const type = `video/${codec}`;
+  selectVideo = () => {
+    const options = {
+      mediaType:'video',
+    };
+    
+    ImagePicker.launchImageLibrary(options, (response) => {
+      // Same code as in above section!
+      const source = { uri: response.uri, video: response.data };
+      this.sendVideoAndConfigToServer(source.uri);
+    });
+  }
 
+  sendVideoAndConfigToServer = async (uri) => {
     const data = new FormData();
+
+    // Append mp4 video
+    const codec = 'H264';
+    const type = `video/${codec}`;
     data.append('file', {
       name: 'video.mp4',
       type,
       uri,
     });
 
+    // Append json config
     const config = {
       is_sample: false,
       compare_to: 'sample_video_snipped.mp4',
     };
-
     const configJson = JSON.stringify(config);
-
     const jsonPath = RNFS.DocumentDirectoryPath + '/config.json';
 
-    // write the file
+    // Write json to the file
     await RNFS.writeFile(jsonPath, configJson, 'utf8');
 
     data.append('file', {
@@ -120,8 +132,17 @@ class CameraView extends Component {
     } catch (e) {
       console.error(e);
     }
+  }
 
-    this.setState({ processing: false });
+  startRecording = async () => {
+    this.setState({ recording: true });
+    // default to mp4 for android as codec is not set
+    const { uri, codec = 'H264' } = await this.camera.recordAsync();
+   
+    const savedUri = this.saveVideoToCameraRoll(uri);
+
+    // this.sendVideoAndConfigToServer(savedUri, codec)
+    this.setState({ recording: false });
   }
 
   stopRecording = () => {
@@ -129,7 +150,7 @@ class CameraView extends Component {
   }
 
   render() {
-    const { recording, processing } = this.state;
+    const { recording } = this.state;
 
     return (
       <>
@@ -145,13 +166,7 @@ class CameraView extends Component {
           style={styles.recordButtonContainer}
         >
           {
-          processing
-            ? (
-              <View style={styles.recordButton}>
-                <ActivityIndicator animating size={18} />
-              </View>
-            )
-            : (
+            <View style={styles.horizontalButtonContainer}>
               <TouchableOpacity
                 onPress={recording ? this.stopRecording : this.startRecording}
                 style={styles.recordButton}
@@ -160,7 +175,15 @@ class CameraView extends Component {
                   {recording ? 'STOP' : 'RECORD'}
                 </Text>
               </TouchableOpacity>
-            )
+              <TouchableOpacity
+                onPress={this.selectVideo}
+                style={styles.recordButton}
+              >
+                <Text style={styles.recordButtonText}>
+                  Select
+                </Text>
+              </TouchableOpacity>
+            </View>
           }
         </View>
       </>
