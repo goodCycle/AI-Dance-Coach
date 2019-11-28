24cd6da373757211f317fd8c23eaa23b316401a9
SimonZocholl <36897182+SimonZocholl@users.noreply.github.com>
2019-11-28 12:35:04 +0900

    fix

--- Backend/build_response/ResponseBuilder.py
+++ Backend/build_response/ResponseBuilder.py
@@ -2,6 +2,14 @@ from build_response.analyze_movement.MovementAnalyzer import MovementAnalyzer
 import cv2
 import os
 import tarfile
+import shutil
+
+
+
+def create_and_clear(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory, ignore_errors=True)
+    os.makedirs(directory, exist_ok=True)
 
 
 class ResponseBuilder:
@@ -14,7 +22,6 @@ class ResponseBuilder:
 
     def build(self):
         self.data = self.analyze(self.input_path)
-
         print(*[(x["frame_number"], x["score"]) for x in self.data])
         threshold = 10
         frame_radius = 5  # number of frames to display before and after
@@ -23,22 +30,18 @@ class ResponseBuilder:
             index_of_failure = next(i for i, val in enumerate(self.data) if val["score"] > threshold)
         except Exception:
             pass
-        print(f'index_of_failure: {index_of_failure}')
         start = index_of_failure - frame_radius if index_of_failure > frame_radius else 0
         # faulty
         end = index_of_failure + frame_radius if len(self.data) > frame_radius + index_of_failure else len(
             self.data) - 1
 
-        print(f'start:{start}, end: {end}')
-
         trial_frames = list(range(start, end + 1))  # is empty
         sample_frames = [self.data[i]["frame_number"] for i in trial_frames]  # is empty
 
-        print(f'trial_frames(1): {trial_frames}')
-        print(f'sample_frames(1):{sample_frames}')
-
         result_dict = self.visualize(trial_frames, sample_frames)
         tar_path = os.path.join(self.result_dir, 'result.tar.gz')
+        # todo: fix
+        create_and_clear(self.result_dir)
         tar = tarfile.open(tar_path, 'w:gz')
         # add json in required
         tar.add(result_dict['sample_result_path'])
@@ -48,32 +51,25 @@ class ResponseBuilder:
 
     # expects 2 lists of file paths to the correct files
     def visualize(self, trial_frames, sample_frames):
-        # todo: build file paths
         sample_dir = os.path.join("./media", self.sample_id, 'skeletons')
         trial_dir = os.path.join('./media', 'temp_vid', 'skeletons')
-        print(f'sample_dir:{sample_dir}, len(sample_frames): {sample_frames}')
-        print(f'trial_dir: {trial_dir}, len(trial_frames): {trial_frames}')
 
         sample_frames = [os.path.join(sample_dir, str(x) + '.jpg') for x in sample_frames]
         trial_frames = [os.path.join(trial_dir, str(x) + '.jpg') for x in trial_frames]
-
-        print(f'sample_frames[0]: {sample_frames[0]}')
-        print(f'trial_frames[0]: {trial_frames[0]}')
 
         array_trial = []
         array_sample = []
         # add path to files if necessary
         for file_trial, file_sample in zip(trial_frames, sample_frames):
-            print(f'file_trial: {file_trial}')
             picture_trial = cv2.imread(file_trial, 1)
             array_trial.append(picture_trial)
-            print(f'file_sample: {file_sample}')
             picture_sample = cv2.imread(file_sample, 1)
             array_sample.append(picture_sample)
 
+        # todo: fix 0 pictures[[0,0,0],[0,0,0],[0,0,0]]
         print(f'len(array_trial): {len(array_trial)}')
-
         print(f'array_trial: {array_trial}')
+
         size = array_trial[0].shape[1::-1]
         # where to store the files?
         trail_result_path = os.path.join(self.result_dir, 'trail.avi')
@@ -89,6 +85,7 @@ class ResponseBuilder:
         # old : (sample_result_path,cv2.VideoWriter_fourcc(*'DIVX'),fps=30, frame_size=size)
         map(out_trail.write, array_sample)
         out_sample.release()
-        return {'sample_result_path': sample_result_path,
-                'trail_result_path': trail_result_path
-                }
+        return {
+            'sample_result_path': sample_result_path,
+            'trail_result_path': trail_result_path
+        }
