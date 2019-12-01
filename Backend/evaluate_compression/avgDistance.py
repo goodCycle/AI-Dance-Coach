7e619396887d96116a226187460222d2517e4e34
hdh112 <avohdh112@gmail.com>
2019-12-01 14:17:04 +0900

    Implemented calculating keypoint distance difference, added image dataset for evaluation

---
+++ Backend/evaluate_compression/avgDistance.py
@@ -0,0 +1,15 @@
+import math
+
+class AvgDistance:
+    def __init__(self, keypoints_a, keypoints_b):
+        self.a = keypoints_a
+        self.b = keypoints_b
+    
+    def calculate(self):
+        count = min(len(self.a), len(self.b))
+        sum = 0
+        for p in range(count):
+            p_a = self.a[p]
+            p_b = self.b[p]
+            sum += math.sqrt((p_a[0] - p_b[0])**2 + (p_a[1] - p_b[1])**2)
+        return sum/count
